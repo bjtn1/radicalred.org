d6218a53b5a5dc7c713014dc26b29f44e4e1414a
Brandon J. T. Noguera <brandonjtnoguera+github@gmail.com>
2024-01-02 19:11:39 -0500

    Update docusaurus config for deployment to netlify

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,11 +13,12 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  // url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
+  // baseUrl: '/',
+  url: 'https://docusaurus-2.netlify.app', // Url to your site with no trailing slash
+  baseUrl: '/', // Base directory of your site relative to your repo
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'facebook', // Usually your GitHub org/user name.
